73ad64a53b542af9a105623bd87b3c3c63600635
Sheralik <Sherali.tgm@gmail.com>
2026-02-20 00:45:12 +0500

    сегодня все

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -72,12 +72,26 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tms',
+        'USER': 'tms_user',
+        'PASSWORD': 'tms123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
+
+
 
 
 # Password validation
@@ -121,6 +135,7 @@ STATIC_URL = 'static/'
 INSTALLED_APPS += [
     'rest_framework',
     'drf_spectacular',
+    'projects',
 ]
 
 REST_FRAMEWORK = {
